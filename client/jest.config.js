7ba56ef5e1c0e5c0de9c3c61ebf0d64f17cc1653
Victor <victor.limac@hotmail.com>
2020-12-14 03:57:27 -0300

    First setup/ First test

---
+++ client/jest.config.js
@@ -0,0 +1,7 @@
+module.export = {
+  testEnviroment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts(x)'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+};
